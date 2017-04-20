f36e0649cf1e7a1ccf169398db434479375bcddb
Chitroopa Manikkavasagam and Marcel Nogueira <chitroopa@>
2017-04-20 12:02:15 -0700

    add rspec

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,15 +1,42 @@
+require('pry')
 class Fixnum
   define_method(:to_coins) do |coin_name|
     cents_string = self.to_s()
     cents_string.concat(coin_name)
   end
   define_method(:coin_combinations) do
-    result_coin = ""
-    if self == 1
-      result_coin = self.to_coins(" Penny")
-    elsif self < 5
-      result_coin = self.to_coins(" Pennies")
+    coin_name_singular = [" Quarter", " Dime", " Nickel", " Penny"]
+    coin_name_plural = [" Quarters", " Dimes", " Nickels", " Pennies"]
+    coin_value = [25,10,5,1]
+      result_coin = []
+    # if self == 1
+    #   result_coin = self.to_coins(" Penny")
+    # elsif self < 5
+    #   result_coin = self.to_coins(" Pennies")
+    # end
+
+    counter = 0
+    reminder = self
+    quotient = 0
+
+    until counter == 4 do
+
+      reminder_temp = reminder % coin_value[counter]
+      quotient = reminder / coin_value[counter]
+
+      if quotient == 1
+        result_coin.push(quotient.to_coins(coin_name_singular[counter]))
+      elsif quotient > 1
+        result_coin.push(quotient.to_coins(coin_name_plural[counter]))
+      end
+
+      counter = counter + 1
+# binding.pry()
+      reminder = reminder_temp
+
     end
-    result_coin
+    result_coin.join(", ")
   end
+
+
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -8,7 +8,16 @@ describe('Fixnum#coin_combinations') do
   it("return in penny if inputted amount is one") do
     expect(1.coin_combinations()).to(eq("1 Penny"))
   end
-  # it("return in penny if inputted amount is one") do
-  #   expect(1.coin_combinations()).to(eq("1 Penny"))
-  # end
+  it("return Quarters for given Cents") do
+    expect(50.coin_combinations()).to(eq("2 Quarters"))
+  end
+  it("return Quarters and dimes for given Cents") do
+    expect(60.coin_combinations()).to(eq("2 Quarters, 1 Dime"))
+  end
+  it("return Quarters, dimes and Nickels for given Cents") do
+    expect(65.coin_combinations()).to(eq("2 Quarters, 1 Dime, 1 Nickel"))
+  end
+  it("return Quarters, dimes,nickels and Pennies for given Cents") do
+    expect(67.coin_combinations()).to(eq("2 Quarters, 1 Dime, 1 Nickel, 2 Pennies"))
+  end
 end
